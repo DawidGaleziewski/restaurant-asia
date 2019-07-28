0f9e918421ed747d711cee4563f6a67d21611155
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-07-28 10:03:50 +0200

    solved issue with nav but code needs refactor

--- app/pre-js/modules/menu.js
+++ app/pre-js/modules/menu.js
@@ -1,47 +1,37 @@
 class Menu{
     start(){
-    // check if device is mobile on load
-    // const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
 
+
+    const btnBurger = document.getElementById('nav-top-burger');
+    const menu = document.querySelector('.nav-top .menu');
+    const menuItems = document.querySelectorAll('.nav-top .menu .menu__item');
+
+    const toggleMobileNav = ()=> {
+        const menu = document.getElementById('nav-menu');
+        const menuItems = document.querySelectorAll('.nav-top .menu .menu__item');
+        menu.classList.toggle('toggle-mobile-nav');
+        btnBurger.classList.toggle('menu-mobile__burger--toggled');
+        menuItems.forEach((menuItem,index)=> {
+            if(menuItem.style.animation){
+                menuItem.style.animation= '';
+            } else{
+                menuItem.style.animation = `linkFading 0.5s ease forwards ${index /7 + 1}s`;
+            }
+        });
+
+    }
         
         // mobile menu logic
         const mobileLogic = ()=> {
-            const btnBurger = document.getElementById('nav-top-burger');
-            const menuButtons = document.querySelectorAll('.nav-top .menu .menu__item');
-
-            const toggleMobileNav = ()=> {
-                const menu = document.getElementById('nav-menu');
-                const menuItems = document.querySelectorAll('.nav-top .menu .menu__item');
-                menu.classList.toggle('toggle-mobile-nav');
-                btnBurger.classList.toggle('menu-mobile__burger--toggled');
-
-                menuItems.forEach((menuItem,index)=> {
-                    if(menuItem.style.animation){
-                        menuItem.style.animation= '';
-                    } else{
-                        menuItem.style.animation = `linkFading 0.5s ease forwards ${index /7 + 1}s`;
-                    }
-                });
-            }
-
-
             btnBurger.addEventListener('click', toggleMobileNav)
-            
-            menuButtons.forEach((button)=>{
-                button.addEventListener('click', toggleMobileNav)
-            })
+            menu.addEventListener('click', toggleMobileNav)
+
+
         }
 
         const desktopLogic = ()=> {
             // Remove mobile effects on desktop
-            const menuButtons = document.querySelectorAll('.nav-top .menu .menu__item');
-            const menuItems = document.querySelectorAll('.nav-top .menu .menu__item');
-
-            menuButtons.forEach((button)=>{
-                button.eventListenerList
-                // button.removeEventListener('click', toggleMobileNav);
-                // need to find a way to remove event listeners only when needed and not all the time
-            })
+            menu.removeEventListener('click', toggleMobileNav);
 
             menuItems.forEach((menuItem,index)=> {
                 menuItem.style.animation= '';
@@ -50,22 +40,28 @@ class Menu{
         }
 
         // If device is displaying with burger menu apply logic for buttons
+        // track the mobile/desktop logic so that eventListiner will be applied only once after the mode has changed
+        let mobileLogicAppliedFlag = false;
         const maxWindthBurgerDisplays = 1019;
         const getWindowWidth = ()=> { return window.innerWidth};
         console.log(getWindowWidth());
         
         const chooseDesktopMobileLogic = ()=> {
-            if (getWindowWidth() <= maxWindthBurgerDisplays ){
+            if (getWindowWidth() <= maxWindthBurgerDisplays && !mobileLogicAppliedFlag ){
                 console.log('mobile logic')
                 mobileLogic()
-            } else {
+                mobileLogicAppliedFlag = true;
+
+            } else if (getWindowWidth() >= maxWindthBurgerDisplays && mobileLogicAppliedFlag ) {
                 console.log('desktop logic')
                 desktopLogic()
+                mobileLogicAppliedFlag = false;
             }
         }
 
         // run once on load so logic will apply even if user will not resize the window
-        chooseDesktopMobileLogic()
+        chooseDesktopMobileLogic();
+        //apply diffrent logic if size changes
         window.onresize = chooseDesktopMobileLogic;
     }
 }
